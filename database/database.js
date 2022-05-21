5f77e59d20e2b24e6eed9eb8945502589bd0b629
amtlib <grzegorz.andrzej.pach@gmail.com>
2022-05-21 14:44:05 +0200

    Update database path

--- database/database.js
+++ database/database.js
@@ -1,4 +1,4 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./score.db', sqlite3.OPEN_READWRITE);
+const db = new sqlite3.Database('./db.sqlite3', sqlite3.OPEN_READWRITE);
 
 module.exports = db;
