20b300be1afa93e7b810a378c6dba3caa16e18ec
amtlib <grzegorz.andrzej.pach@gmail.com>
2022-05-21 14:44:15 +0200

    Register new routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 
 const gamesRouter = require("./routes/games");
+const genresRouter = require("./routes/genres");
 
 const registerRouter = require("./routes/auth/register");
 const loginRouter = require("./routes/auth/login");
@@ -16,6 +17,8 @@ app.get('/', (req, res) => {
 });
 
 app.use("/games", gamesRouter);
+app.use("/genres", genresRouter);
+
 app.use("/register", registerRouter);
 app.use("/login", loginRouter);
 
