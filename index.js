c880829301b8526791a66d659ce2731abb71f3bd
amtlib <grzegorz.andrzej.pach@gmail.com>
2022-05-22 10:47:34 +0200

    Use cookie-parser and add users route

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
 const gamesRouter = require("./routes/games");
-const genresRouter = require("./routes/genres");
-const platformsRouter = require("./routes/platforms");
 const guestbookRouter = require("./routes/guestbookEntries");
+const usersRouter = require("./routes/users")
 
 const registerRouter = require("./routes/auth/register");
 const loginRouter = require("./routes/auth/login");
@@ -12,19 +12,20 @@ const loginRouter = require("./routes/auth/login");
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('nothing to see here');
 });
 
 app.use("/games", gamesRouter);
-app.use("/genres", genresRouter);
-app.use("/platforms", platformsRouter);
 app.use("/guestbook", guestbookRouter);
 
 app.use("/register", registerRouter);
 app.use("/login", loginRouter);
+
+app.use("/users", usersRouter)
 
 app.listen(port, () => {
   console.log(`Score API listening on port ${port}`)
